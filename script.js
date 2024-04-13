d13071d99df4300f9d3b44b6bf95496f65da713e
Dionte Driskell <driskell06@gmail.com>
2024-04-12 22:58:40 -0400

    #1part1

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const PI = 3.1415;
-const area = PI * radius * radius;
+// const PI = 3.1415;
+// const area = PI * radius * radius;
 // Each plant requires a minimum space of 0.8 square meters.
 // The area is starting with 20 plants.
 // The plants double in number every week.
 // plant growth after a specific number of weeks.
-let AreaOfCircle = (r)=> 5 
-    let pie = 3.14
-    let area = (pie * r * r
-    let = 
-    console.log (AreaOfCircle);
+let AreaOfCircle = (r) => 5
+const pie = 3.14;
+let area = (pie * r * r;
+let plantSpace = 0.8; squareMeters 
+let totalPlants = 20;
 
+for (let weeks = 1); weeks <= 4; weeks++) {
+    for (let i = 0; i < weeks; i++) {
+        totalPlants * = 2
+    }
+area * = 2;
+const freeSpace = area - (totalPlants * plantSpace);
+const newGrowth = (freeSpace / plantSpace);
+totalPlants += newGrowth;
 
-let areaStart = 20 plants
-let plantPpace = 0.8 squareMeters
-let plantWeeks = * 2
 }
-console.log (pie * r * r)
+console.log ("number of weeks, totalPlants");
+
+    // console.log(AreaOfCircle);
+
+
+
+// let areaStart = 20 plants
+// let plantSpace = 0.8 squareMeters
+// let plantWeeks = * 2
+// }
+// console.log(pie * r * r)
 
 // let x = 5
 
